46f451d7a1fbc0cc1e775cdaf290e1be058230db
Luke Lancaster <luketlancaster@gmail.com>
2015-06-06 18:07:20 -0500

    reconcile differences in honolulu and nashville views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,10 @@ class HomeController < ApplicationController
         :title=>data['title']
       )
     end
-    @nash_chart_data = Dataset.where("identifier like '%nashville%'").pluck(:title, :date_created, :date_updated)
-    @nash_count = Dataset.where("identifier like '%nashville%'").count
-    @created_this_month = Dataset.where(:date_created => 1.month.ago..Time.now).pluck(:title, :category)
-    @updated_this_month = Dataset.where(:date_updated => 1.month.ago..Time.now).pluck(:title, :category)
+    nashville_data = Dataset.where("identifier like '%nashville%'")
+    @nash_chart_data = nashville_data.pluck(:title, :date_created, :date_updated)
+    @nash_count = nashville_data.count
+    @created_this_month = nashville_data.where(:date_created => 1.month.ago..Time.now).pluck(:title, :category)
+    @updated_this_month = nashville_data.where(:date_updated => 1.month.ago..Time.now).pluck(:title, :category)
   end
 end
--- app/controllers/honolulu_controller.rb
+++ app/controllers/honolulu_controller.rb
@@ -5,10 +5,14 @@ class HonoluluController < ApplicationController
         Dataset.where(:identifier=>data['identifier']).first_or_create(
           :date_created=>data['issued'],
           :date_updated=>data['modified'],
+          :category=>data['landingPage'],
           :title=>data['title']
         )
       end
-      @honolulu_chart_data = Dataset.where("identifier like '%honolulu%'").pluck(:title, :date_created, :date_updated)
-      @honolulu_count = Dataset.where("identifier like '%honolulu%'").count
+      honolulu_data = Dataset.where("identifier like '%honolulu%'")
+      @honolulu_chart_data = honolulu_data.pluck(:title, :date_created, :date_updated)
+      @honolulu_count = honolulu_data.count
+      @created_this_month = honolulu_data.where(:date_created => 1.month.ago..Time.now).pluck(:title, :category)
+      @updated_this_month = honolulu_data.where(:date_updated => 1.month.ago..Time.now).pluck(:title, :category)
   end
 end
