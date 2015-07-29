e3f5947517b23f4d0c30c383ca9a537380cf7e16
Max Shenfield <shenfieldmax@gmail.com>
2015-07-28 23:27:58 -0500

    Removed chartkick. Starting setting up data for treechart
    Removed chartkick. Chartkick has limited support for configuration and advanced usage of charts. It also doesn't have an interface for the treechart. Google Charts is straightforward enough, and the documentation is complete, so I'm planning on handrolling it see
    
    Started setting up data for treechart. The treechart requires a parent/child relationship. I started by removing an extra branch in the index route of the home_controller. Then I grabbed an appropriate field to group by ("Theme" which corresponds to category). Still have to group the data, generate summary statistics, and create the treechart server side.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,36 +1,21 @@
 class HomeController < ApplicationController
   def index
-    if !params["/"].nil? and params["/"]["dataSet"].present?
-      api_data = HTTParty.get("https://data.#{params['/']['dataSet']}.gov/data.json", :verify => false)
-      api_data['dataset'].each do |data|
-        Dataset.where(:identifier=>data['identifier']).first_or_create(
-          :date_created=>data['issued'],
-          :date_updated=>data['modified'],
-          :category=>data['landingPage'],
-          :title=>data['title']
-        )
-      end
-      data = Dataset.where("identifier like '%#{params['/']['dataSet']}%'")
-      @city = params["/"]["dataSet"]
-      @chart_data = data.pluck(:title, :date_created, :date_updated)
-      @count = data.count
-      @created_this_month = data.where(:date_created => 1.month.ago..Time.now).pluck(:title, :category)
-      @updated_this_month = data.where(:date_updated => 1.month.ago..Time.now).pluck(:title, :category)
-    else
-      api_data = HTTParty.get('https://data.nashville.gov/data.json')
-      api_data['dataset'].each do |data|
-        Dataset.where(:identifier=>data['identifier']).first_or_create(
-          :date_created=>data['issued'],
-          :date_updated=>data['modified'],
-          :category=>data['landingPage'],
-          :title=>data['title']
-        )
-      end
-      data = Dataset.where("identifier like '%nashville%'")
-      @chart_data = data.pluck(:title, :date_created, :date_updated)
-      @count = data.count
-      @created_this_month = data.where(:date_created => 1.month.ago..Time.now).pluck(:title, :category)
-      @updated_this_month = data.where(:date_updated => 1.month.ago..Time.now).pluck(:title, :category)
+    # Nashville by default
+    @city = (!params["/"].nil? and params["/"]["dataSet"].present?) ? params['/']['dataSet'] : "nashville"
+    api_data = HTTParty.get("https://data.#{@city}.gov/data.json", :verify => false)
+    api_data['dataset'].each do |data|
+      Dataset.where(:identifier=>data['identifier']).first_or_create(
+        :date_created=>data['issued'],
+        :date_updated=>data['modified'],
+        :category=>data['theme'][0],
+        :title=>data['title']
+      )
     end
+    data = Dataset.where("identifier like '%#{@city}%'")
+    categories = data.pluck(:category).uniq
+    @chart_data = data.pluck(:title, :date_created, :date_updated)
+    @count = data.count
+    @created_this_month = data.where(:date_created => 1.month.ago..Time.now).pluck(:title, :category)
+    @updated_this_month = data.where(:date_updated => 1.month.ago..Time.now).pluck(:title, :category)
   end
 end
